91a92958b40b0cd9a3342b2d0539440a8873db90
Emrah SÖZLÜ <emrahsozlu@aof.anadolu.edu.tr>
2023-09-13 10:24:20 +0300

    Entities and IRepository added.

---
+++ src/ApplicationCore/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string ProductName { get; set; } = null!;
+
+        public string? ProductDescription { get; set; } = null!;
+
+        public decimal ProductPrice { get; set; }
+
+        public string? PictureUri { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int BrandId { get; set; }
+
+
+        public Category Category { get; set; } = null!;
+
+        public Brand Brand { get; set; } = null!;
+    }
+}
