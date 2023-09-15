9c8b915d4dc89c7fead950a1f2d2107fe52934f3
Emrah SÖZLÜ <emrahsozlu@aof.anadolu.edu.tr>
2023-09-15 11:29:35 +0300

    Home category, brand filter added.

---
+++ src/ApplicationCore/Specifications/ProductFilterSpecification.cs
@@ -0,0 +1,22 @@
+﻿using ApplicationCore.Entities;
+using Ardalis.Specification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Specifications
+{
+    public class ProductFilterSpecification : Specification<Product>
+    {
+        public ProductFilterSpecification(int? categoryId, int? brandId)
+        {
+            if (categoryId.HasValue)
+                Query.Where(c => c.CategoryId == categoryId);
+
+            if (brandId.HasValue)
+                Query.Where(b => b.BrandId == brandId);
+        }
+    }
+}
--- src/Web/Services/HomeViewModelService.cs
+++ src/Web/Services/HomeViewModelService.cs
@@ -1,26 +1,43 @@
-﻿namespace Web.Services
+﻿using ApplicationCore.Specifications;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Web.Services
 {
     public class HomeViewModelService : IHomeViewModelService
     {
         private readonly IRepository<Product> _productRepo;
+        private readonly IRepository<Category> _categoryRepo;
+        private readonly IRepository<Brand> _brandRepo;
 
-        public HomeViewModelService(IRepository<Product> productRepo)
+        public HomeViewModelService(IRepository<Product> productRepo, IRepository<Category> categoryRepo, IRepository<Brand> brandRepo)
         {
             _productRepo = productRepo;
+            _categoryRepo = categoryRepo;
+            _brandRepo = brandRepo;
         }
 
-        public async Task<HomeViewModel> GetHomeViewModelAsync()
+        public async Task<HomeViewModel> GetHomeViewModelAsync(int? categoryId, int? brandId)
         {
+            var specProducts = new ProductFilterSpecification(categoryId, brandId);
+            var products = await _productRepo.GetAllAsync(specProducts);
+
             var vm = new HomeViewModel()
             {
-                Products = (await _productRepo.GetAllAsync()).Select(pr =>
+                Products = products.Select(pr =>
                 new ProductViewModel()
                 {
                     Id = pr.Id,
                     ProductName = pr.ProductName,
                     ProductPrice = pr.ProductPrice,
                     PictureUri = pr.PictureUri
-                }).ToList()
+                }).ToList(),
+
+                Categories = (await _categoryRepo.GetAllAsync()).Select(cr =>
+                    new SelectListItem(cr.CategoryName, cr.Id.ToString())).ToList(),
+                Brands = (await _brandRepo.GetAllAsync()).Select(br =>
+                    new SelectListItem(br.BrandName, br.Id.ToString())).ToList(),
+                CategoryId = categoryId,
+                BrandId = brandId
             };
 
             return vm;
