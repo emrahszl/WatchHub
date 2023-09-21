e3586486e73d607739dfe50ec61e823c927966c7
Emrah SÖZLÜ <emrahsozlu@aof.anadolu.edu.tr>
2023-09-21 10:17:01 +0300

    Adress, Order and OrderItem added.

---
+++ src/ApplicationCore/Entities/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities
+{
+    public class Order : BaseEntity
+    {
+        public int BuyerId { get; set; }
+
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
+
+        public Address ShipToAdress { get; set; } = null!;
+
+
+        public List<OrderItem> OrderItems { get; set; } = new();
+    }
+}
